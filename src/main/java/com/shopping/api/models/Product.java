805851f343855e70c39925413fdd3216444b600a
Rodolfo Sousa <rodolfozsousa@gmail.com>
2023-04-05 23:46:07 -0300

    feat: add product model

---
+++ src/main/java/com/shopping/api/models/Product.java
@@ -0,0 +1,27 @@
+package com.shopping.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Products")
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private Long price;
+
+  @Column(length = 100, nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String imageUrl;
+}
