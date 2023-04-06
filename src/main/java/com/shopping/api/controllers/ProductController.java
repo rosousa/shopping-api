5d5c694606cacc4712c0f188865f285e997f1cd3
Rodolfo Sousa <rodolfozsousa@gmail.com>
2023-04-06 00:29:38 -0300

    feat: add route get product by id

--- src/main/java/com/shopping/api/controllers/ProductController.java
+++ src/main/java/com/shopping/api/controllers/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,11 @@ public class ProductController {
   public List<Product> getProducts() {
     List<Product> products = repository.findAll();
     return products;
+  }
+
+  @GetMapping("/{id}")
+  public Product getProduct(@PathVariable Long id) {
+    Product product = repository.findById(id).orElse(null);
+    return product;
   }
 }
